3506ebeea9874d7122cc85d1f18d0063579b836e
BuildTools <unconfigured@null.spigotmc.org>
2017-11-07 08:03:26 +0900

    ScriptBlockPlus v1.4.2

--- src/main/java/com/github/yuttyann/scriptblockplus/PlayerSelector.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/PlayerSelector.java
@@ -1,35 +1,118 @@
 package com.github.yuttyann.scriptblockplus;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
+import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 
-import com.github.yuttyann.scriptblockplus.utils.StreamUtils;
+import com.github.yuttyann.scriptblockplus.enums.PackageType;
 import com.github.yuttyann.scriptblockplus.utils.StringUtils;
 import com.github.yuttyann.scriptblockplus.utils.Utils;
 
-public class PlayerSelector {
+public final class PlayerSelector {
 
-	private static final Pattern a = Pattern.compile("^@([parf])(?:\\[([\\w=,!-]*)\\])?$");
-	private static final Pattern b = Pattern.compile("\\G([-!]?[\\w-]*)(?:$|,)");
-	private static final Pattern c = Pattern.compile("\\G(\\w+)=([-!]?[\\w-]*)(?:$|,)");
+	private static final Object[] OBJECT_ARRAY = ArrayUtils.EMPTY_OBJECT_ARRAY;
+	private static final Class<?>[] CLASS_ARRAY = ArrayUtils.EMPTY_CLASS_ARRAY;
+
+	private static Map<String, Object> map = null;
+
+	static final class TileEntityCommand {
+
+		private static Object newInstance() throws ReflectiveOperationException {
+			return ((Class<?>) map.get("Class_TileEntityCommand")).newInstance();
+		}
+
+		private static void setWorld(Object titleEntityCommand, World world) throws ReflectiveOperationException {
+			Method setWorld = (Method) map.get("TileEntityCommand_setWorld");
+			Method getHandle = (Method) map.get("CraftWorld_getHandle");
+			setWorld.invoke(titleEntityCommand, getHandle.invoke(world, OBJECT_ARRAY));
+		}
+
+		private static void setLocation(Object titleEntityCommand, Location location) throws ReflectiveOperationException {
+			int x = location.getBlockX();
+			int y = location.getBlockY();
+			int z = location.getBlockZ();
+			Object setLocation = map.get("TileEntityCommand_setLocation");
+			if (setLocation instanceof Method) {
+				Method setBlockPosition = (Method) setLocation;
+				Constructor<?> constructor = ((Constructor<?>) map.get("Constructor_BlockPosition"));
+				setBlockPosition.invoke(titleEntityCommand, constructor.newInstance(x, y, z));
+			} else {
+				Field[] xyz = (Field[]) setLocation;
+				xyz[0].set(titleEntityCommand, x);
+				xyz[1].set(titleEntityCommand, y);
+				xyz[2].set(titleEntityCommand, z);
+			}
+		}
+
+		private static Object getCommandBlock(Object titleEntityCommand) throws ReflectiveOperationException {
+			Method getCommandBlock = (Method) map.get("TileEntityCommand_getCommandBlock");
+			return getCommandBlock.invoke(titleEntityCommand, OBJECT_ARRAY);
+		}
+	}
+
+	protected static void load() throws ReflectiveOperationException {
+		map = new HashMap<String, Object>();
+		map.put("Pattern", Pattern.compile("^@([parf])(?:\\[([\\w=,!-]*)\\])?$"));
+
+		Class<?> nmsEntity = PackageType.MINECRAFT_SERVER.getClass("Entity");
+		if (Utils.isCB18orLater()) {
+			map.put("Class_NMSEntity", nmsEntity);
+		}
+		map.put("NMSEntity_getBukkitEntity", nmsEntity.getMethod("getBukkitEntity", CLASS_ARRAY));
+
+		Class<?> iCommandListener = PackageType.MINECRAFT_SERVER.getClass("ICommandListener");
+		Class<?> playerSelector = PackageType.MINECRAFT_SERVER.getClass("PlayerSelector");
+		Method getPlayers;
+		if (Utils.isCB18orLater()) {
+			getPlayers = playerSelector.getMethod("getPlayers", iCommandListener, String.class, Class.class);
+		} else {
+			getPlayers = playerSelector.getMethod("getPlayers", iCommandListener, String.class);
+		}
+		map.put("PlayerSelector_getPlayers", getPlayers);
+
+		Class<?> tileEntityCommand = PackageType.MINECRAFT_SERVER.getClass("TileEntityCommand");
+		Class<?> craftWorld = PackageType.CRAFTBUKKIT.getClass("CraftWorld");
+		Class<?> world = PackageType.MINECRAFT_SERVER.getClass("World");
+		map.put("Class_TileEntityCommand", tileEntityCommand);
+		map.put("CraftWorld_getHandle", craftWorld.getMethod("getHandle", CLASS_ARRAY));
+		map.put("TileEntityCommand_setWorld", tileEntityCommand.getMethod("a", world));
+
+		Object setLocation;
+		if (Utils.isCB18orLater()) {
+			Class<?> blockPosition = PackageType.MINECRAFT_SERVER.getClass("BlockPosition");
+			String methodName = Utils.isCB110orLater() ? "setPosition" : "a";
+			setLocation = tileEntityCommand.getMethod(methodName, blockPosition);
+			map.put("Constructor_BlockPosition", blockPosition.getConstructor(int.class, int.class, int.class));
+		} else {
+			Field[] xyz = new Field[] {
+				tileEntityCommand.getField("x"),
+				tileEntityCommand.getField("y"),
+				tileEntityCommand.getField("z")
+			};
+			setLocation = xyz;
+		}
+		map.put("TileEntityCommand_setLocation", setLocation);
+
+		Method getCommandBlock;
+		if (Utils.isCB1710orLater()) {
+			getCommandBlock = tileEntityCommand.getMethod("getCommandBlock", CLASS_ARRAY);
+		} else {
+			getCommandBlock = tileEntityCommand.getMethod("a", CLASS_ARRAY);
+		}
+		map.put("TileEntityCommand_getCommandBlock", getCommandBlock);
+	}
 
 	public static String getCommandBlockPattern(String command) {
 		String[] array = StringUtils.split(command, " ");
@@ -41,297 +124,65 @@ public class PlayerSelector {
 		return null;
 	}
 
-	public static Player[] getPlayers(Location location, String s) {
-		Matcher matcher = a.matcher(s);
-		if (!matcher.matches()) {
-			return null;
-		}
-		Map<String, String> map = h(matcher.group(2));
-		String s1 = matcher.group(1);
-		int i = c(s1);
-		int j = d(s1);
-		int k = f(s1);
-		int l = e(s1);
-		int i1 = g(s1);
-		int j1 = -1;
-		GameMode mode = null;
-		Location loc = null;
-		if (location != null) {
-			loc = location.clone();
-		}
-		Map<String, Integer> map1 = a(map);
-		String s2 = null;
-		String s3 = null;
-		boolean flag = false;
-		if (map.containsKey("rm")) {
-			i = a((String) map.get("rm"), i);
-			flag = true;
-		}
-		if (map.containsKey("r")) {
-			j = a((String) map.get("r"), j);
-			flag = true;
-		}
-		if (map.containsKey("lm")) {
-			k = a((String) map.get("lm"), k);
-		}
-		if (map.containsKey("l")) {
-			l = a((String) map.get("l"), l);
-		}
-		if (map.containsKey("x") && location != null) {
-			loc.setX(a((String) map.get("x"), location.getX()));
-			flag = true;
-		}
-		if (map.containsKey("y") && location != null) {
-			loc.setY(a((String) map.get("y"), location.getY()));
-			flag = true;
-		}
-		if (map.containsKey("z") && location != null) {
-			loc.setZ(a((String) map.get("z"), location.getZ()));
-			flag = true;
-		}
-		if (map.containsKey("m")) {
-			j1 = a((String) map.get("m"), j1);
-			if (j1 == 0) {
-				mode = GameMode.SURVIVAL;
-			} else if (j1 == 1) {
-				mode = GameMode.CREATIVE;
-			} else if (j1 == 2) {
-				mode = GameMode.ADVENTURE;
-			} else if (Utils.isCB18orLater() && j1 == 3) {
-				mode = GameMode.SPECTATOR;
-			}
-		}
-		if (map.containsKey("c")) {
-			i1 = a((String) map.get("c"), i1);
-		}
-		if (map.containsKey("team")) {
-			s3 = (String) map.get("team");
-		}
-		if (map.containsKey("name")) {
-			s2 = (String) map.get("name");
-		}
-		World world = flag ? location.getWorld() : null;
-		List<Player> list;
-		if (!s1.equals("p") && !s1.equals("a")) {
-			if (!s1.equals("r")) {
-				return null;
-			}
-			list = a(loc, i, j, 0, mode, k, l, map1, s2, s3, world);
-			Collections.shuffle(list);
-			list = list.subList(0, Math.min(i1, list.size()));
-			return list != null && !list.isEmpty() ? list.toArray(new Player[list.size()]) : new Player[0];
-		} else {
-			list = a(loc, i, j, i1, mode, k, l, map1, s2, s3, world);
-			return list != null && !list.isEmpty() ? list.toArray(new Player[list.size()]) : new Player[0];
-		}
-	}
-
-	private static Map<String, Integer> a(Map<String, String> map) {
-		HashMap<String, Integer> hashmap = new HashMap<String, Integer>(map.size());
-		StreamUtils.filterForEach(map.keySet(),
-			s -> s.startsWith("score_") && s.length() > "score_".length(),
-				s -> hashmap.put(s.substring("score_".length()), Integer.valueOf(a(map.get(s), 1))));
-		return hashmap;
-	}
-
-	public static boolean isList(String s) {
-		Matcher matcher = a.matcher(s);
-		if (!matcher.matches()) {
-			return false;
-		}
-		Map<String, String> map = h(matcher.group(2));
-		String s1 = matcher.group(1);
-		int i = g(s1);
-		if (map.containsKey("c")) {
-			i = a(map.get("c"), i);
-		}
-		return i != 1;
-	}
-
 	public static boolean isPattern(String s, String s1) {
-		Matcher matcher = a.matcher(s);
+		Matcher matcher = ((Pattern) map.get("Pattern")).matcher(s);
 		if (matcher.matches()) {
 			String s2 = matcher.group(1);
 			return s1 == null || s1.equals(s2);
-		} else {
-			return false;
 		}
+		return false;
 	}
 
 	public static boolean isPattern(String s) {
 		return isPattern(s, (String) null);
 	}
 
-	private static final int c(String s) {
-		return 0;
-	}
-
-	private static final int d(String s) {
-		return 0;
-	}
-
-	private static final int e(String s) {
-		return Integer.MAX_VALUE;
-	}
-
-	private static final int f(String s) {
-		return 0;
-	}
-
-	private static final int g(String s) {
-		return s.equals("a") ? 0 : 1;
-	}
-
-	private static Map<String, String> h(String s) {
-		HashMap<String, String> hashmap = new HashMap<String, String>();
-		if (s == null) {
-			return hashmap;
-		}
-		Matcher matcher = b.matcher(s);
-		int i = 0;
-		int j;
-		for (j = -1; matcher.find(); j = matcher.end()) {
-			String s1 = null;
-			switch (i++) {
-			case 0:
-				s1 = "x";
-				break;
-			case 1:
-				s1 = "y";
-				break;
-			case 2:
-				s1 = "z";
-				break;
-			case 3:
-				s1 = "r";
-			}
-			if (s1 != null && matcher.group(1).length() > 0) {
-				hashmap.put(s1, matcher.group(1));
-			}
-		}
-		if (j < s.length()) {
-			matcher = c.matcher(j == -1 ? s : s.substring(j));
-			while (matcher.find()) {
-				hashmap.put(matcher.group(1), matcher.group(2));
-			}
-		}
-		return hashmap;
-	}
-
-	private static int a(String s, int i) {
-		int j = i;
+	public static Player[] getPlayers(Location location, String s) {
 		try {
-			j = Integer.parseInt(s);
-		} catch (Throwable t) {}
-		return j;
+			Object iCommandListener = getBlockCommandListener(location);
+			if (iCommandListener != null) {
+				List<Player> players = getPlayers(iCommandListener, s);
+				return players.size() > 0 ? players.toArray(new Player[players.size()]) : new Player[0];
+			}
+		} catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	private static double a(String s, double d0) {
-		double d1 = d0;
-		try {
-			d1 = Double.parseDouble(s);
-		} catch (Throwable t) {}
-		return d1;
-	}
-
-	private static List<Player> a(final Location location, int i, int j, int k, GameMode l, int i1, int j1, Map<String, Integer> map, String s, String s1, World world) {
-		List<Player> list = new ArrayList<Player>();
-		boolean flag = k < 0;
-		boolean flag1 = s != null && s.startsWith("!");
-		boolean flag2 = s1 != null && s1.startsWith("!");
-		k = k >= 0 ? k : -k;
-		if (flag1) {
-			s = s.substring(1);
-		}
-		if (flag2) {
-			s1 = s1.substring(1);
-		}
-		for (Player player : Bukkit.getOnlinePlayers()) {
-			if ((world == null || player.getWorld() == world)
-					&& (s == null || flag1 != s.equalsIgnoreCase(player.getName()))) {
-				if (s1 != null) {
-					Scoreboard sb = player.getScoreboard();
-					@SuppressWarnings("deprecation")
-					Team team = sb.getPlayerTeam(player);
-					String s2 = team == null ? "" : team.getName();
-					if (flag2 == s1.equalsIgnoreCase(s2)) {
-						continue;
-					}
-				}
-				if (location != null && (i > 0 || j > 0)) {
-					double f = location.distance(player.getLocation());
-					if (i > 0 && f < i || j > 0 && f > j) {
-						continue;
-					}
-				}
-				if (a(player, map) && (l == null || l == player.getGameMode()) && (i1 <= 0 || player.getLevel() >= i1) && player.getLevel() <= j1) {
-					list.add(player);
+	private static List<Player> getPlayers(Object iCommandListener, String s) throws ReflectiveOperationException {
+		Method getPlayers = (Method) map.get("PlayerSelector_getPlayers");
+		List<Player> players;
+		if (Utils.isCB18orLater()) {;
+			Class<?> class_NMSEntity = (Class<?>) map.get("Class_NMSEntity");
+			List<?> entities = (List<?>) getPlayers.invoke(null, iCommandListener, s, class_NMSEntity);
+			players = new ArrayList<Player>(entities.size());
+			for (Object nmsEntity : entities) {
+				Entity entity = (Entity) getBukkitEntity(nmsEntity);
+				if (entity instanceof Player) {
+					players.add((Player) entity);
 				}
 			}
-		}
-		if (location != null) {
-			Collections.sort(list, new Comparator<Player>() {
-				public int compare(Player p1, Player p2) {
-					double d1 = p1.getLocation().distance(location);
-					double d2 = p2.getLocation().distance(location);
-					if (d1 < d2) {
-						return -1;
-					} else if (d1 > d2) {
-						return 1;
-					}
-					return 0;
-				}
-			});
-		}
-		if (flag) {
-			Collections.reverse(list);
-		}
-		if (k > 0) {
-			list = list.subList(0, Math.min(k, list.size()));
-		}
-		return list;
-	}
-
-	private static boolean a(Player player, Map<String, Integer> map) {
-		if (map == null || map.size() == 0) {
-			return true;
-		}
-		Iterator<Entry<String, Integer>> iterator = map.entrySet().iterator();
-		Entry<String, Integer> entry;
-		boolean flag;
-		int i;
-		do {
-			if (!iterator.hasNext()) {
-				return true;
-			}
-			entry = iterator.next();
-			String s = entry.getKey();
-			flag = false;
-			if (s.endsWith("_min") && s.length() > 4) {
-				flag = true;
-				s = s.substring(0, s.length() - 4);
-			}
-			Scoreboard scoreboard = player.getScoreboard();
-			Objective objective = scoreboard.getObjective(s);
-			if (objective == null) {
-				return false;
-			}
-			Score score = getScore(objective, player);
-			i = score.getScore();
-			if (i < (entry.getValue()).intValue() && flag) {
-				return false;
-			}
-		} while (i <= (entry.getValue()).intValue() || flag);
-		return false;
-	}
-
-	private static Score getScore(Objective objective, Player player) {
-		if (Utils.isCB178orLater()) {
-			return objective.getScore(player.getName());
 		} else {
-			@SuppressWarnings("deprecation")
-			Score score = objective.getScore(player);
-			return score;
+			Object[] entities = (Object[]) getPlayers.invoke(null, iCommandListener, s);
+			players = new ArrayList<Player>(entities.length);
+			for (Object nmsEntity : entities) {
+				players.add((Player) getBukkitEntity(nmsEntity));
+			}
 		}
+		return players;
+	}
+
+	private static Object getBukkitEntity(Object nmsEntity) throws ReflectiveOperationException {
+		Method getBukkitEntity = (Method) map.get("NMSEntity_getBukkitEntity");
+		Object entity = getBukkitEntity.invoke(nmsEntity, OBJECT_ARRAY);
+		return Utils.isCB18orLater() ? (Entity) entity : (Player) entity;
+	}
+
+	private static Object getBlockCommandListener(Location location) throws ReflectiveOperationException {
+		Object titleEntityCommand = TileEntityCommand.newInstance();
+		TileEntityCommand.setWorld(titleEntityCommand, location.getWorld());
+		TileEntityCommand.setLocation(titleEntityCommand, location);
+		return TileEntityCommand.getCommandBlock(titleEntityCommand);
 	}
 }
