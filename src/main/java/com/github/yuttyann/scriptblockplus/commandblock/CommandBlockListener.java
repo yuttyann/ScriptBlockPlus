5afcbdcf9c0a9bfbe624c5945bc1d84fae0a8f26
BuildTools <unconfigured@null.spigotmc.org>
2017-12-10 07:35:49 +0900

    no message

--- src/main/java/com/github/yuttyann/scriptblockplus/commandblock/CommandBlockListener.java
+++
@@ -1,9 +0,0 @@
-package com.github.yuttyann.scriptblockplus.commandblock;
-
-import org.bukkit.Location;
-import org.bukkit.command.CommandSender;
-
-public interface CommandBlockListener {
-
-	public void executeCommand(CommandSender sender, Location location, String command);
-}
--- src/main/java/com/github/yuttyann/scriptblockplus/script/endprocess/EndProcess.java
+++
@@ -1,8 +0,0 @@
-package com.github.yuttyann.scriptblockplus.script.endprocess;
-
-public interface EndProcess {
-
-	public void success(SBRead sbRead);
-
-	public void failed(SBRead sbRead);
-}
