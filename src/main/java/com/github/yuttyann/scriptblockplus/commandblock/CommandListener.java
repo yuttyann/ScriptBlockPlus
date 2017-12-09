4aac262c33127f8341967b07e9a9c4fa59751a11
BuildTools <unconfigured@null.spigotmc.org>
2017-12-10 07:32:04 +0900

    ScriptBlockPlus v1.4.8

--- src/main/java/com/github/yuttyann/scriptblockplus/commandblock/CommandListener.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/commandblock/CommandListener.java
@@ -3,7 +3,7 @@ package com.github.yuttyann.scriptblockplus.commandblock;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 
-public interface CommandBlockListener {
+public interface CommandListener {
 
 	public void executeCommand(CommandSender sender, Location location, String command);
 }
