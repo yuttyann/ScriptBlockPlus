b6ef2e69ae1ab02e416a3521185545eb34a0d71e
yuttyann44581 <misuto546@gmail.com>
2018-07-16 18:00:21 +0900

    【v1.5.1】 リフレクションをMinecraft v1.13へ対応（未検証）

--- src/main/java/com/github/yuttyann/scriptblockplus/commandblock/Ref_Vx_x_Rx.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/commandblock/Ref_Vx_x_Rx.java
@@ -11,16 +11,18 @@ import com.github.yuttyann.scriptblockplus.utils.Utils;
 
 class Ref_Vx_x_Rx implements CommandListener {
 
-	private static final Class<?>[] PARAMS_EXECUTE_COMMAND = {
-		getClass(PackageType.NMS, "ICommandListener"), CommandSender.class, String.class
+	private static final String[] CLASS_NAME = {
+			"ICommandListener", "CommandBlockListenerAbstract", "TileEntityCommand",
+			"CommandDispatcher", "ChatComponentText", "BlockPosition", "CraftServer", "CraftWorld"
 	};
 
-	private static final String CLASS_NAME_1 = "CommandBlockListenerAbstract";
-	private static final String CLASS_NAME_2 = "TileEntityCommand";
+	private static final Class<?>[] PARAMS_EXECUTE_COMMAND = {
+			getClass(PackageType.NMS, CLASS_NAME[0]), CommandSender.class, String.class
+	};
 
 	@Override
 	@Deprecated
-	public boolean executeCommand(CommandSender sender, Location location, String command) {
+	public final boolean executeCommand(CommandSender sender, Location location, String command) {
 		try {
 			Object iCommandListener = getICommandListener(sender, location);
 			return executeCommand(iCommandListener, sender, command) > 0;
@@ -31,11 +33,21 @@ class Ref_Vx_x_Rx implements CommandListener {
 	}
 
 	protected int executeCommand(Object sender, CommandSender bSender, String command) throws ReflectiveOperationException {
-		Method executeCommand = PackageType.NMS.getMethod(CLASS_NAME_1, "executeCommand", PARAMS_EXECUTE_COMMAND);
-		return Integer.class.cast(executeCommand.invoke(null, sender, bSender, command)).intValue();
+		int completed = 0;
+		if (Utils.isCBXXXorLater("1.13")) {
+			Object wrapper = PackageType.NMS.invokeMethod(sender, CLASS_NAME[1], "getWrapper", (Object[]) null);
+			Object server = PackageType.CB.invokeMethod(bSender.getServer(), CLASS_NAME[6], "getServer", (Object[]) null);
+			Object dispatcher = PackageType.NMS.invokeMethod(server, CLASS_NAME[6], "getCommandDispatcher", (Object[]) null);
+			Object dispatch = PackageType.NMS.invokeMethod(dispatcher, CLASS_NAME[3], "dispatchServerCommand", wrapper, command);
+			completed = Integer.class.cast(dispatch).intValue();
+		} else {
+			Method executeCommand = PackageType.NMS.getMethod(CLASS_NAME[1], "executeCommand", PARAMS_EXECUTE_COMMAND);
+			completed = Integer.class.cast(executeCommand.invoke(null, sender, bSender, command)).intValue();
+		}
+		return completed;
 	}
 
-	private Object getICommandListener(CommandSender sender, Location location) throws ReflectiveOperationException {
+	protected Object getICommandListener(CommandSender sender, Location location) throws ReflectiveOperationException {
 		Object titleEntityCommand = newTileEntityCommand();
 		setWorld(titleEntityCommand, location.getWorld());
 		setLocation(titleEntityCommand, location);
@@ -46,42 +58,47 @@ class Ref_Vx_x_Rx implements CommandListener {
 		return commandListener;
 	}
 
-	private void setName(Object commandListener, String name) throws ReflectiveOperationException {
+	protected void setName(Object commandListener, String name) throws ReflectiveOperationException {
 		String methodName = Utils.isCBXXXorLater("1.7.10") ? "setName" : "b";
-		PackageType.NMS.invokeMethod(commandListener, CLASS_NAME_1, methodName, name);
+		if (Utils.isCBXXXorLater("1.13")) {
+			Object cName = PackageType.NMS.newInstance(CLASS_NAME[4], name);
+			PackageType.NMS.invokeMethod(commandListener, CLASS_NAME[1], methodName, cName);
+		} else {
+			PackageType.NMS.invokeMethod(commandListener, CLASS_NAME[1], methodName, name);
+		}
 	}
 
-	private Object newTileEntityCommand() throws ReflectiveOperationException {
-		return PackageType.NMS.newInstance(CLASS_NAME_2, (Object[]) null);
+	protected Object newTileEntityCommand() throws ReflectiveOperationException {
+		return PackageType.NMS.newInstance(CLASS_NAME[1], (Object[]) null);
 	}
 
-	private void setWorld(Object titleEntityCommand, World world) throws ReflectiveOperationException {
-		Object nmsWorld = PackageType.CB.invokeMethod(world, "CraftWorld", "getHandle", (Object[]) null);
+	protected void setWorld(Object titleEntityCommand, World world) throws ReflectiveOperationException {
+		Object nmsWorld = PackageType.CB.invokeMethod(world, CLASS_NAME[7], "getHandle", (Object[]) null);
 		Class<?> nmsWorldClass = PackageType.NMS.getClass("World");
-		PackageType.NMS.getMethod(CLASS_NAME_2, "a", nmsWorldClass).invoke(titleEntityCommand, nmsWorld);
+		PackageType.NMS.getMethod(CLASS_NAME[2], "a", nmsWorldClass).invoke(titleEntityCommand, nmsWorld);
 	}
 
-	private void setLocation(Object titleEntityCommand, Location location) throws ReflectiveOperationException {
+	protected void setLocation(Object titleEntityCommand, Location location) throws ReflectiveOperationException {
 		int x = location.getBlockX();
 		int y = location.getBlockY();
 		int z = location.getBlockZ();
 		if (Utils.isCBXXXorLater("1.8")) {
 			String methodName = Utils.isCBXXXorLater("1.10") ? "setPosition" : "a";
-			Object instance = PackageType.NMS.newInstance("BlockPosition", x, y, z);
-			PackageType.NMS.invokeMethod(titleEntityCommand, CLASS_NAME_2, methodName, instance);
+			Object instance = PackageType.NMS.newInstance(CLASS_NAME[5], x, y, z);
+			PackageType.NMS.invokeMethod(titleEntityCommand, CLASS_NAME[2], methodName, instance);
 		} else {
-			PackageType.NMS.setFieldValue(CLASS_NAME_2, "x", titleEntityCommand, x);
-			PackageType.NMS.setFieldValue(CLASS_NAME_2, "y", titleEntityCommand, y);
-			PackageType.NMS.setFieldValue(CLASS_NAME_2, "z", titleEntityCommand, z);
+			PackageType.NMS.setFieldValue(CLASS_NAME[2], "x", titleEntityCommand, x);
+			PackageType.NMS.setFieldValue(CLASS_NAME[2], "y", titleEntityCommand, y);
+			PackageType.NMS.setFieldValue(CLASS_NAME[2], "z", titleEntityCommand, z);
 		}
 	}
 
-	private Object getCommandBlock(Object titleEntityCommand) throws ReflectiveOperationException {
+	protected Object getCommandBlock(Object titleEntityCommand) throws ReflectiveOperationException {
 		String methodName = Utils.isCBXXXorLater("1.7.10") ? "getCommandBlock" : "a";
-		return PackageType.NMS.invokeMethod(titleEntityCommand, CLASS_NAME_2, methodName, (Object[]) null);
+		return PackageType.NMS.invokeMethod(titleEntityCommand, CLASS_NAME[2], methodName, (Object[]) null);
 	}
 
-	private static Class<?> getClass(PackageType packageType, String className) {
+	protected static Class<?> getClass(PackageType packageType, String className) {
 		try {
 			return packageType.getClass(className);
 		} catch (IllegalArgumentException | ClassNotFoundException e) {
