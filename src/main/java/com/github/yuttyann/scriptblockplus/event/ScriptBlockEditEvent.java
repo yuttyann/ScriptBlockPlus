fe8bf3c3962b2b604d00c55ad3eac3757159cd22
yuttyann44581 <misuto546@gmail.com>
2018-08-09 08:00:48 +0900

    【v1.5.1】 最適化または大きな変更なし

--- src/main/java/com/github/yuttyann/scriptblockplus/event/ScriptBlockEditEvent.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/event/ScriptBlockEditEvent.java
@@ -8,20 +8,14 @@ import com.github.yuttyann.scriptblockplus.script.ScriptType;
 
 public class ScriptBlockEditEvent extends ScriptBlockEvent {
 
-	private String[] actionArray;
 	private ScriptType scriptType;
 	private ActionType actionType;
 	private boolean cancelled;
 
 	public ScriptBlockEditEvent(Player player, Block block, String[] actionArray) {
 		super(player, block);
-		this.actionArray = actionArray;
 		this.scriptType = ScriptType.valueOf(actionArray[0]);
 		this.actionType = ActionType.valueOf(actionArray[1]);
-	}
-
-	public String[] getArray() {
-		return actionArray;
 	}
 
 	public ScriptType getScriptType() {
