4140755e8605a0764856b65793df46afc46b732b
yuttyann44581 <misuto546@gmail.com>
2020-02-23 11:57:43 +0900

    [v1.8.2] getNameを削除

--- src/main/java/com/github/yuttyann/scriptblockplus/command/BaseCommand.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/command/BaseCommand.java
@@ -9,6 +9,8 @@ import org.bukkit.command.ProxiedCommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import com.github.yuttyann.scriptblockplus.enums.Permission;
 import com.github.yuttyann.scriptblockplus.file.SBConfig;
@@ -20,19 +22,20 @@ public abstract class BaseCommand extends CommandUsage implements TabExecutor {
 	private Plugin plugin;
 	private boolean isIgnoreUsage;
 
-	public BaseCommand(Plugin plugin) {
+	public BaseCommand(@NotNull Plugin plugin) {
 		this.plugin = plugin;
 		setUsage(getUsages());
 	}
 
+	@NotNull
 	public final Plugin getPlugin() {
 		return plugin;
 	}
 
-	public abstract String getName();
-
+	@NotNull
 	public abstract String getCommandName();
 
+	@Nullable
 	public abstract CommandData[] getUsages();
 
 	public abstract boolean isAliases();
@@ -42,7 +45,7 @@ public abstract class BaseCommand extends CommandUsage implements TabExecutor {
 	}
 
 	@Override
-	public final boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	public final boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 		if (Utils.isCBXXXorLater("1.8.3") && sender instanceof ProxiedCommandSender) {
 			CommandSender proxiedCommandSender = ((ProxiedCommandSender) sender).getCallee();
 			if (proxiedCommandSender instanceof Player) {
@@ -60,21 +63,22 @@ public abstract class BaseCommand extends CommandUsage implements TabExecutor {
 	}
 
 	@Override
-	public final List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+	@NotNull
+	public final List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 		List<String> completeList = new ArrayList<String>();
 		tabComplete(sender, command, label, args, completeList);
 		return completeList;
 	}
 
-	protected abstract boolean runCommand(CommandSender sender, Command command, String label, String[] args);
+	protected abstract boolean runCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args);
 
-	protected abstract void tabComplete(CommandSender sender, Command command, String label, String[] args, List<String> empty);
+	protected abstract void tabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args, @NotNull List<String> empty);
 
-	protected final boolean hasPermission(CommandSender sender, Permission permission) {
+	protected final boolean hasPermission(@NotNull CommandSender sender, @NotNull Permission permission) {
 		return hasPermission(sender, permission, true);
 	}
 
-	protected final boolean hasPermission(CommandSender sender, Permission permission, boolean isPlayer) {
+	protected final boolean hasPermission(@NotNull CommandSender sender, @NotNull Permission permission, boolean isPlayer) {
 		if (isPlayer && !isPlayer(sender)) {
 			return false;
 		}
@@ -86,7 +90,7 @@ public abstract class BaseCommand extends CommandUsage implements TabExecutor {
 		return has;
 	}
 
-	protected final boolean isPlayer(CommandSender sender) {
+	protected final boolean isPlayer(@Nullable CommandSender sender) {
 		if (sender instanceof Player) {
 			return true;
 		}
@@ -95,11 +99,11 @@ public abstract class BaseCommand extends CommandUsage implements TabExecutor {
 		return false;
 	}
 
-	protected final boolean equals(String source, String another) {
-		return another == null ? false : another.equalsIgnoreCase(source);
+	protected final boolean equals(@NotNull String source, @NotNull String another) {
+		return another.equalsIgnoreCase(source);
 	}
 
-	protected final boolean equals(String source, String... anothers) {
+	protected final boolean equals(@NotNull String source, @NotNull String... anothers) {
 		return StreamUtils.anyMatch(anothers, s -> equals(source, s));
 	}
 }
