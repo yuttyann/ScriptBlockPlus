22246cd80b1140c76887df2d51521e16b0985b00
yuttyann44581 <misuto546@gmail.com>
2020-03-13 19:22:24 +0900

    [v1.8.4] 修正

--- src/main/java/com/github/yuttyann/scriptblockplus/enums/TextOption.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/enums/TextOption.java
@@ -18,10 +18,12 @@ public enum TextOption {
 		return name;
 	}
 
+	@NotNull
 	public String replace(@NotNull String source, @NotNull String value) {
 		return StringUtils.replace(source, name, value);
 	}
 
+	@NotNull
 	public static String replaceAll(@NotNull String source, @NotNull SBPlayer sbPlayer) {
 		if (StringUtils.isNotEmpty(source)) {
 			for (TextOption option : values()) {
