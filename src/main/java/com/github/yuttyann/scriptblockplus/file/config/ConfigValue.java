6b58c0f7540f43ce1e9e094bb2c7768da038b5a8
yuttyann44581 <misuto546@gmail.com>
2020-03-19 05:30:18 +0900

    消し忘れ

--- src/main/java/com/github/yuttyann/scriptblockplus/file/config/ConfigValue.java
+++
@@ -1,9 +0,0 @@
-package com.github.yuttyann.scriptblockplus.file.config;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface ConfigValue<T> {
-
-    @NotNull
-    T value(@NotNull T value);
-}
