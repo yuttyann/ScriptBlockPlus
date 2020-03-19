2fee07b8f83a7ea6c7a562675955ce96c6a3fb4c
yuttyann44581 <misuto546@gmail.com>
2020-03-19 22:46:45 +0900

    [v1.8.6] ScriptEditの改善に伴い、一部メソッドを変更

--- src/main/java/com/github/yuttyann/scriptblockplus/script/SBClipboard.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/script/SBClipboard.java
@@ -1,22 +1,18 @@
 package com.github.yuttyann.scriptblockplus.script;
 
-import com.github.yuttyann.scriptblockplus.player.SBPlayer;
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
 
 public interface SBClipboard {
 
-	public void save();
-
-	@NotNull
-	public Location getLocation();
-
 	@NotNull
 	public ScriptType getScriptType();
 
-	public boolean copy(@NotNull SBPlayer sbPlayer);
+	public void save();
+
+	public boolean copy();
 
 	public boolean paste(@NotNull Location location, boolean overwrite);
 
-	public boolean lightPaste(@NotNull Location location, boolean overwrite, boolean updateTime);
+	public boolean lightPaste(@NotNull Location location, boolean overwrite);
 }
