d0649ecd77fa70bd4060ccdba346b1ab259c59f7
yuttyann44581 <misuto546@gmail.com>
2020-03-24 05:25:41 +0900

    [v1.8.7] クラス名を変更

--- src/main/java/com/github/yuttyann/scriptblockplus/script/ScriptMap.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/script/ScriptMap.java
@@ -9,20 +9,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public abstract class ScriptObjectMap extends ScriptListener implements ObjectMap {
+abstract class ScriptMap extends ScriptListener implements ObjectMap {
 
 	private static final Map<UUID, Map<String, Object>> OBJECT_MAP = new HashMap<>();
 
 	private final UUID ramdomId = UUID.randomUUID();
 
-	protected ScriptObjectMap(@NotNull ScriptListener listener) {
+	ScriptMap(@NotNull ScriptListener listener) {
 		super(listener);
 	}
 
 	@Override
 	public void put(@NotNull String key, @Nullable Object value) {
-		Map<String, Object> map = OBJECT_MAP.computeIfAbsent(ramdomId, k -> new HashMap<>());
-		map.put(key, value);
+		OBJECT_MAP.computeIfAbsent(ramdomId, k -> new HashMap<>()).put(key, value);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -33,24 +32,9 @@ public abstract class ScriptObjectMap extends ScriptListener implements ObjectMa
 		return map == null ? null : (T) map.get(key);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	@Nullable
-	public <T> T remove(@NotNull String key) {
-		Map<String, Object> map = OBJECT_MAP.get(ramdomId);
-		return map == null ? null : (T) map.remove(key);
-	}
-
-	@Override
-	public boolean containsKey(@NotNull String key) {
-		Map<String, Object> map = OBJECT_MAP.get(ramdomId);
-		return map != null && map.containsKey(key);
-	}
-
-	@Override
-	public boolean containsValue(@NotNull Object value) {
-		Map<String, Object> map = OBJECT_MAP.get(ramdomId);
-		return map != null && map.containsValue(value);
+	public void remove(@NotNull String key) {
+		OBJECT_MAP.get(ramdomId).remove(key);
 	}
 
 	@Override
