01a626aef6f08020d9227fea3a52482dfa1ea363
yuttyann44581 <misuto546@gmail.com>
2020-04-20 16:32:54 +0900

    [1.8.9] VERY_HIGH を追加

--- src/main/java/com/github/yuttyann/scriptblockplus/enums/OptionPriority.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/enums/OptionPriority.java
@@ -5,6 +5,7 @@ import com.github.yuttyann.scriptblockplus.script.option.other.Amount;
 import com.github.yuttyann.scriptblockplus.script.option.other.Calculation;
 import com.github.yuttyann.scriptblockplus.script.option.other.Execute;
 import com.github.yuttyann.scriptblockplus.script.option.other.ScriptAction;
+import com.github.yuttyann.scriptblockplus.script.option.time.Delay;
 import com.github.yuttyann.scriptblockplus.script.option.vault.MoneyCost;
 import com.github.yuttyann.scriptblockplus.script.option.vault.PermRemove;
 import org.jetbrains.annotations.NotNull;
@@ -39,6 +40,11 @@ public enum OptionPriority {
      * 優先位置: {@link MoneyCost} の後ろ
      */
     HIGH(new MoneyCost().getSyntax()),
+
+    /**
+     * 優先位置: {@link Delay} の後ろ
+     */
+    VERY_HIGH(new Delay().getSyntax()),
 
     /**
      * 優先位置: {@link Calculation} の後ろ
