219fa0b7962138b19e745c0ea2a542f63f89f11d
yuttyann44581 <misuto546@gmail.com>
2020-05-03 08:19:36 +0900

    [v1.9.0] 文字列の比較ができる仕様に変更

--- src/main/java/com/github/yuttyann/scriptblockplus/script/option/other/Calculation.java
+++ src/main/java/com/github/yuttyann/scriptblockplus/script/option/other/Calculation.java
@@ -2,6 +2,7 @@ package com.github.yuttyann.scriptblockplus.script.option.other;
 
 import com.github.yuttyann.scriptblockplus.BlockCoords;
 import com.github.yuttyann.scriptblockplus.enums.reflection.PackageType;
+import com.github.yuttyann.scriptblockplus.script.ScriptType;
 import com.github.yuttyann.scriptblockplus.script.hook.HookPlugins;
 import com.github.yuttyann.scriptblockplus.script.hook.VaultEconomy;
 import com.github.yuttyann.scriptblockplus.script.option.BaseOption;
@@ -42,8 +43,9 @@ public class Calculation extends BaseOption {
 	@Override
 	protected boolean isValid() throws Exception {
 		String[] array = StringUtils.split(getOptionValue(), " ");
-		double value1 = parse(array[0]);
-		double value2 = parse(array[2]);
+		Player player = getPlayer();
+		Object value1 = parse(player, array[0]);
+		Object value2 = parse(player, array[2]);
 		String operator = array[1];
 
 		if (result(value1, value2, operator)) {
@@ -54,133 +56,138 @@ public class Calculation extends BaseOption {
 			message = StringUtils.replace(message, "%value1%", value1);
 			message = StringUtils.replace(message, "%value2%", value2);
 			message = StringUtils.replace(message, "%operator%", operator);
-			Utils.sendColorMessage(getSBPlayer(), message);
+			Utils.sendColorMessage(player, message);
 		}
 		return false;
 	}
 
-	private double parse(@NotNull String source) throws Exception {
+	private Object parse(Player player, @NotNull String source) throws Exception {
 		if (REALNUMBER_PATTERN.matcher(source).matches()) {
 			return Double.parseDouble(source);
 		}
-		return getValue(getPlayer(), source);
-	}
-
-	private double getValue(@NotNull Player player, @NotNull String variable) throws Exception {
-		if (variable.startsWith("%player_others_in_range_") && variable.endsWith("%")) {
-			variable = variable.substring("%player_others_in_range_".length(), variable.length() - 1);
-			return getNearbyOthers(player, Integer.parseInt(variable));
+		if (source.startsWith("%player_count_") && source.endsWith("%")) {
+			source = source.substring("%player__".length(), source.length() - 1);
+			String[] array = StringUtils.split(source, ",");
+			if (array.length < 1 || array.length > 2) {
+				return 0;
+			}
+			ScriptType scriptType = array.length == 1 ? getScriptType() : ScriptType.valueOf(array[0]);
+			BlockCoords blockCoords = BlockCoords.fromString(array.length == 1 ? array[0] : array[1]);
+			return getSBPlayer().getPlayerCount().getInfo(blockCoords, scriptType).getAmount();
 		}
-		if (variable.startsWith("%player_count_") && variable.endsWith("%")) {
-			variable = variable.substring("%player_count_".length(), variable.length() - 1);
-			BlockCoords blockCoords = BlockCoords.fromString(variable);
-			return getSBPlayer().getPlayerCount().getInfo(blockCoords, getScriptType()).getAmount();
+		if (source.startsWith("%player_others_in_range_") && source.endsWith("%")) {
+			source = source.substring("%player_others_in_range_".length(), source.length() - 1);
+			return getNearbyOthers(player, Integer.parseInt(source));
 		}
-		if (variable.startsWith("%player_ping_") && variable.endsWith("%")) {
-			variable = variable.substring("%player_ping_".length(), variable.length() - 1);
-			Player target = Bukkit.getPlayer(variable);
-			if (target == null) {
-				return 0.0D;
+		if (source.startsWith("%player_ping_") && source.endsWith("%")) {
+			source = source.substring("%player_ping_".length(), source.length() - 1);
+			Player target = Bukkit.getPlayer(source);
+			if (target == null || !Utils.isPlatform()) {
+				return 0;
 			}
 			Object handle = PackageType.CB_ENTITY.invokeMethod(target, "CraftPlayer", "getHandle");
 			return PackageType.NMS.getField("EntityPlayer", "ping").getInt(handle);
 		}
-		if (variable.startsWith("%server_online_") && variable.endsWith("%")) {
-			variable = variable.substring("%server_online_".length(), variable.length() - 1);
-			return Objects.requireNonNull(Utils.getWorld(variable)).getPlayers().size();
+		if (source.startsWith("%server_online_") && source.endsWith("%")) {
+			source = source.substring("%server_online_".length(), source.length() - 1);
+			return Objects.requireNonNull(Utils.getWorld(source)).getPlayers().size();
 		}
-		if (variable.startsWith("%objective_score_") && variable.endsWith("%")) {
-			variable = variable.substring("%objective_score_".length(), variable.length() - 1);
+		if (source.startsWith("%objective_score_") && source.endsWith("%")) {
+			source = source.substring("%objective_score_".length(), source.length() - 1);
 			ScoreboardManager scoreboardManager = Objects.requireNonNull(Bukkit.getScoreboardManager());
-			Objective objective = scoreboardManager.getMainScoreboard().getObjective(variable);
-			return objective == null ? 0.0D : getScore(objective, player).getScore();
+			Objective objective = scoreboardManager.getMainScoreboard().getObjective(source);
+			return objective == null ? 0 : getScore(objective, player).getScore();
 		}
-		switch (variable) {
-		case "%server_online%":
-			return Bukkit.getOnlinePlayers().size();
-		case "%server_offline%":
-			return Bukkit.getOfflinePlayers().length;
-		case "%player_count%":
-			BlockCoords fullCoords = BlockCoords.fromString(getFullCoords());
-			return getSBPlayer().getPlayerCount().getInfo(fullCoords, getScriptType()).getAmount();
-		case "%player_ping%":
-			Object handle = player.getClass().getMethod("getHandle").invoke(player);
-			return (int) handle.getClass().getField("ping").get(handle);
-		case "%player_x%":
-			return player.getLocation().getBlockX();
-		case "%player_y%":
-			return player.getLocation().getBlockY();
-		case "%player_z%":
-			return player.getLocation().getBlockZ();
-		case "%player_bed_x%":
-			return player.getBedSpawnLocation() == null ? 0.0D : player.getBedSpawnLocation().getBlockX();
-		case "%player_bed_y%":
-			return player.getBedSpawnLocation() == null ? 0.0D : player.getBedSpawnLocation().getBlockY();
-		case "%player_bed_z%":
-			return player.getBedSpawnLocation() == null ? 0.0D : player.getBedSpawnLocation().getBlockZ();
-		case "%player_compass_x%":
-			return player.getCompassTarget().getBlockX();
-		case "%player_compass_y%":
-			return player.getCompassTarget().getBlockY();
-		case "%player_compass_z%":
-			return player.getCompassTarget().getBlockZ();
-		case "%player_gamemode%":
-			@SuppressWarnings("deprecation")
-			int value = player.getGameMode().getValue();
-			return value;
-		case "%player_world_time%":
-			return player.getWorld().getTime();
-		case "%player_exp%":
-			return player.getExp();
-		case "%player_exp_to_level%":
-			return player.getExpToLevel();
-		case "%player_level%":
-			return player.getLevel();
-		case "%player_fly_speed%":
-			return player.getFlySpeed();
-		case "%player_food_level%":
-			return player.getFoodLevel();
-		case "%player_health%":
-			return player.getHealth();
-		case "%player_health_scale%":
-			return player.getHealthScale();
-		case "%player_last_damage%":
-			return player.getLastDamage();
-		case "%player_max_health%":
-			@SuppressWarnings("deprecation")
-			double health = player.getMaxHealth();
-			return health;
-		case "%player_max_air%":
-			return player.getMaximumAir();
-		case "%player_max_no_damage_ticks%":
-			return player.getMaximumNoDamageTicks();
-		case "%player_no_damage_ticks%":
-			return player.getNoDamageTicks();
-		case "%player_time%":
-			return player.getPlayerTime();
-		case "%player_time_offset%":
-			return player.getPlayerTimeOffset();
-		case "%player_remaining_air%":
-			return player.getRemainingAir();
-		case "%player_saturation%":
-			return player.getSaturation();
-		case "%player_sleep_ticks%":
-			return player.getSleepTicks();
-		case "%player_ticks_lived%":
-			return player.getTicksLived();
-		case "%player_seconds_lived%":
-			return player.getTicksLived() * 20D;
-		case "%player_minutes_lived%":
-			return (player.getTicksLived() * 20D) / 60D;
-		case "%player_total_exp%":
-			return player.getTotalExperience();
-		case "%player_walk_speed%":
-			return player.getWalkSpeed();
-		case "%vault_eco_balance%":
-			VaultEconomy vaultEconomy = HookPlugins.getVaultEconomy();
-			return vaultEconomy.isEnabled() ? vaultEconomy.getBalance(player) : 0.0D;
-		default:
-			return Double.parseDouble(setPlaceholders(getPlayer(), variable));
+		switch (source) {
+			case "%server_online%":
+				return Bukkit.getOnlinePlayers().size();
+			case "%server_offline%":
+				return Bukkit.getOfflinePlayers().length;
+			case "%player_count%":
+				BlockCoords fullCoords = BlockCoords.fromString(getFullCoords());
+				return getSBPlayer().getPlayerCount().getInfo(fullCoords, getScriptType()).getAmount();
+			case "%player_ping%":
+				if (!Utils.isPlatform()) {
+					return 0;
+				}
+				Object handle = player.getClass().getMethod("getHandle").invoke(player);
+				return handle.getClass().getField("ping").get(handle);
+			case "%player_x%":
+				return player.getLocation().getBlockX();
+			case "%player_y%":
+				return player.getLocation().getBlockY();
+			case "%player_z%":
+				return player.getLocation().getBlockZ();
+			case "%player_bed_x%":
+				return player.getBedSpawnLocation() == null ? 0 : player.getBedSpawnLocation().getBlockX();
+			case "%player_bed_y%":
+				return player.getBedSpawnLocation() == null ? 0 : player.getBedSpawnLocation().getBlockY();
+			case "%player_bed_z%":
+				return player.getBedSpawnLocation() == null ? 0 : player.getBedSpawnLocation().getBlockZ();
+			case "%player_compass_x%":
+				return player.getCompassTarget().getBlockX();
+			case "%player_compass_y%":
+				return player.getCompassTarget().getBlockY();
+			case "%player_compass_z%":
+				return player.getCompassTarget().getBlockZ();
+			case "%player_gamemode%":
+				@SuppressWarnings("deprecation")
+				int value = player.getGameMode().getValue();
+				return value;
+			case "%player_world_time%":
+				return player.getWorld().getTime();
+			case "%player_exp%":
+				return player.getExp();
+			case "%player_exp_to_level%":
+				return player.getExpToLevel();
+			case "%player_level%":
+				return player.getLevel();
+			case "%player_fly_speed%":
+				return player.getFlySpeed();
+			case "%player_food_level%":
+				return player.getFoodLevel();
+			case "%player_health%":
+				return player.getHealth();
+			case "%player_health_scale%":
+				return player.getHealthScale();
+			case "%player_last_damage%":
+				return player.getLastDamage();
+			case "%player_max_health%":
+				@SuppressWarnings("deprecation")
+				double health = player.getMaxHealth();
+				return health;
+			case "%player_max_air%":
+				return player.getMaximumAir();
+			case "%player_max_no_damage_ticks%":
+				return player.getMaximumNoDamageTicks();
+			case "%player_no_damage_ticks%":
+				return player.getNoDamageTicks();
+			case "%player_time%":
+				return player.getPlayerTime();
+			case "%player_time_offset%":
+				return player.getPlayerTimeOffset();
+			case "%player_remaining_air%":
+				return player.getRemainingAir();
+			case "%player_saturation%":
+				return player.getSaturation();
+			case "%player_sleep_ticks%":
+				return player.getSleepTicks();
+			case "%player_ticks_lived%":
+				return player.getTicksLived();
+			case "%player_seconds_lived%":
+				return player.getTicksLived() * 20;
+			case "%player_minutes_lived%":
+				return (player.getTicksLived() * 20) / 60;
+			case "%player_total_exp%":
+				return player.getTotalExperience();
+			case "%player_walk_speed%":
+				return player.getWalkSpeed();
+			case "%vault_eco_balance%":
+				VaultEconomy vaultEconomy = HookPlugins.getVaultEconomy();
+				return vaultEconomy.isEnabled() ? vaultEconomy.getBalance(player) : 0;
+			default:
+				String placeholder = setPlaceholders(getPlayer(), source);
+				return REALNUMBER_PATTERN.matcher(placeholder).matches() ? Double.parseDouble(placeholder) : placeholder;
 		}
 	}
 
@@ -215,20 +222,25 @@ public class Calculation extends BaseOption {
 		return objective.getScore(player.getName());
 	}
 
-	private boolean result(double value1, double value2, @NotNull String operator) {
+	private boolean result(Object value1, Object value2, @NotNull String operator) {
+		if (!(value1 instanceof Number && value2 instanceof Number)) {
+			return operator.equals("==") && Objects.equals(value1, value2);
+		}
+		double v1 = Double.parseDouble(value1.toString());
+		double v2 = Double.parseDouble(value2.toString());
 		switch (operator) {
 		case "<":
-			return value1 < value2;
+			return v1 < v2;
 		case "<=":
-			return value1 <= value2;
+			return v1 <= v2;
 		case ">":
-			return value1 > value2;
+			return v1 > v2;
 		case ">=":
-			return value1 >= value2;
+			return v1 >= v2;
 		case "==":
-			return value1 == value2;
+			return v1 == v2;
 		case "!=":
-			return value1 != value2;
+			return v1 != v2;
 		default:
 			return false;
 		}
