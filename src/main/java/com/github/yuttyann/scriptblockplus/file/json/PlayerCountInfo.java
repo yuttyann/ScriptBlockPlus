b283f68e741421a3caaa37980be3ae1fa81e428b
yuttyann44581 <misuto546@gmail.com>
2020-05-05 23:04:21 +0900

    [v1.9.1] パッケージの場所を移動

---
+++ src/main/java/com/github/yuttyann/scriptblockplus/file/json/PlayerCountInfo.java
@@ -0,0 +1,85 @@
+package com.github.yuttyann.scriptblockplus.file.json;
+
+import com.github.yuttyann.scriptblockplus.script.ScriptType;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * ScriptBlockPlus PlayerCountInfo クラス
+ * @author yuttyann44581
+ */
+public class PlayerCountInfo {
+
+	@SerializedName("fullcoords")
+	@Expose
+	private final String fullCoords;
+
+	@SerializedName("scripttype")
+	@Expose
+	private final ScriptType scriptType;
+
+	@SerializedName("amount")
+	@Expose
+	private int amount;
+
+	public PlayerCountInfo(@NotNull String fullCoords, @NotNull ScriptType scriptType) {
+		this.fullCoords = fullCoords;
+		this.scriptType = scriptType;
+	}
+
+	public int add() {
+		synchronized(this) {
+			return ++amount;
+		}
+	}
+
+	public int subtract() {
+		synchronized(this) {
+			return amount < 1 ? --amount : 0;
+		}
+	}
+
+	public void setAmount(int amount) {
+		synchronized(this) {
+			this.amount = Math.min(amount, 0);
+		}
+	}
+
+	public synchronized int getAmount() {
+		return amount;
+	}
+
+	@NotNull
+	public String getFullCoords() {
+		return fullCoords;
+	}
+
+	@NotNull
+	public ScriptType getScriptType() {
+		return scriptType;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 1;
+		int prime = 31;
+		hash = prime * hash + fullCoords.hashCode();
+		hash = prime * hash + scriptType.hashCode();
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof PlayerCountInfo) {
+			PlayerCountInfo info = ((PlayerCountInfo) obj);
+			return info.scriptType.equals(scriptType) && info.fullCoords.equals(fullCoords);
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "PlayerCountInfo{fullCoords=" + fullCoords + ", scriptType=" + scriptType + ", amount=" + amount + "}";
+	}
+}
