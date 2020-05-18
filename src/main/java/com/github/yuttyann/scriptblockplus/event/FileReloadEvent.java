9d0960c951f408963e5785f0fc1cf8dfbb87c545
yuttyann44581 <misuto546@gmail.com>
2020-05-18 10:34:48 +0900

    [v1.9.3] ファイルをリロードした際に発生するイベントを追加

---
+++ src/main/java/com/github/yuttyann/scriptblockplus/event/FileReloadEvent.java
@@ -0,0 +1,21 @@
+package com.github.yuttyann.scriptblockplus.event;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class FileReloadEvent extends Event {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
