539c29b607385a195da7524326e1ae9c68f2a73f
yuttyann44581 <misuto546@gmail.com>
2020-06-12 16:30:18 +0900

    [v1.9.5] アイテムの設定を変更できないようにするクラス

---
+++ src/main/java/com/github/yuttyann/scriptblockplus/listener/item/UnmodifiableItemStack.java
@@ -0,0 +1,69 @@
+package com.github.yuttyann.scriptblockplus.listener.item;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+public final class UnmodifiableItemStack extends ItemStack {
+
+    protected UnmodifiableItemStack(@NotNull ItemStack item) {
+        super(item);
+    }
+
+    @Override
+    public void setType(@NotNull Material type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setData(@Nullable MaterialData data) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @Deprecated
+    public void setDurability(short durability) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addEnchantment(@NotNull Enchantment ench, int level) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addEnchantments(@NotNull Map<Enchantment, Integer> enchantments) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addUnsafeEnchantment(@NotNull Enchantment ench, int level) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addUnsafeEnchantments(@NotNull Map<Enchantment, Integer> enchantments) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int removeEnchantment(@NotNull Enchantment ench) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean setItemMeta(@Nullable ItemMeta meta) {
+        throw new UnsupportedOperationException();
+    }
+}
